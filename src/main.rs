627a36d9e128d4d25f872d004765959f9883be10
Chris Davison <c.jr.davison@gmail.com>
2023-03-02 12:42:43 +0000

    write to file by flag

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ mod timer;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "timer")]
 struct Opts {
-    #[structopt(short, long)]
-    newline: bool,
+    #[structopt(short="f", long="file")]
+    file: Option<String>,
     message: Vec<String>,
     #[structopt(short = "h", long, default_value = "0")]
     hours: u64,
@@ -29,17 +29,32 @@ fn main() -> Result<(), std::io::Error> {
             Some(args.message.join(" "))
         },
     );
-    let reset = if args.newline {
-        String::from("\n")
-    } else {
-        format!("\r{}\r", " ".repeat(format!("{}", timer).len()))
-    };
+    let reset = format!("\r{}\r", " ".repeat(format!("{}", timer).len()));
     let mut stdout = io::stdout();
     while !timer.finished() {
-        print!("{}{}", reset, timer);
-        stdout.flush()?;
+        match &args.file {
+            Some(fname) => {
+                let mut file = std::fs::OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .open(fname)?;
+                file.write_all(format!("{timer}").as_bytes())?;
+            },
+            _ => {
+                stdout.write_all(format!("{reset}{timer}").as_bytes())?;
+                stdout.flush()?;
+            }
+        }
         timer.tick();
     }
+    if let Some(fname) = args.file {
+        let mut file = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(fname)?;
+        file.write_all(format!("Done.\n").as_bytes())?;
+    }
+
     println!("{}DONE.", reset);
     Ok(())
 }
